d66ef382de1445570e0c73c875d44545b9d62873
himmelherz <nozarishi@gmail.com>
2016-07-22 23:18:52 +0300

    Add good help and AllCost command

--- Main.java
+++ Main.java
@@ -1,88 +1,159 @@
 package task2_1.main;
+
 import task2_1.officesupply.OfficeSupply;
+
 import task2_1.worker.Worker;
+
 import java.util.Scanner;
+
+	
 	
 
 public class Main
-{
 
-	public static void AddWorker(Worker[] worker)
 	{
-		
-		Scanner scan = new Scanner(System.in);
-		int k =0;
-		for(int i=0; i< worker.length;i++)
-		{
-			
-			if(worker[i].name.equals("") && (k == 0))
-			{
-				k = 1;
-				System.out.println("Enter name of worker: ");
-				String s = scan.nextLine();
-				worker[i].name = s;
-				worker[i].Fill();
-			}
-		}
-	}
 
-	public static void main(String[] args) 
-		{	int counter = 0;
+	
+		public static void AddWorker(Worker[] worker)
+	
+		{
+		
+		
+			Scanner scan = new Scanner(System.in);
+		
+			int k =0;
+		
+			for(int i=0; i< worker.length;i++)
+		
+			{
+			
+			
+				if(worker[i].name.equals("") && (k == 0))
+			
+				{
+				
+					k = 1;
+				
+					System.out.println("Enter name of worker: ");
+				
+					String s = scan.nextLine();
+				
+					worker[i].name = s;
+				
+					worker[i].Fill();
+			
+				}
+		
+			}
+	
+		}
+
+	
+		public static void main(String[] args) 
+					
+		{	
+			int counter = 0;
+			
 			OfficeSupply supply = new OfficeSupply();
+			
 			OfficeSupply pen_for_examine = new OfficeSupply();	
 
+			
 			
 			Worker worker[] = new Worker[100];
 				
 		
-			for(int i=0;i<100;i++)
-						{
-							worker[i] = new Worker();;
-						}
-			Scanner scan = new Scanner(System.in);
-			while(counter == 0)
-			{
-				System.out.println("Enter your request");
-				String s = scan.nextLine();
-				switch (s) 
-				{
-    				case "show all workers":  
-					{
-						for(int i=0;i<10;i++)
-						{
-							worker[i].ShowHim();
-						}
-             					break;
-					}
 				
-    				case "add worker":  
-					{
+			for(int i=0;i<100;i++)
 						
-						AddWorker(worker);
-						System.out.println("Worker was created");
-             					break;
-					}
-				case "show worker cost":  
+			{
+							
+				worker[i] = new Worker();
+						
+			}
+			
+		Scanner scan = new Scanner(System.in);
+			
+		while(counter == 0)
+			
+		{
+				
+			System.out.println("Enter your request");
+				
+			String s = scan.nextLine();
+				
+			switch (s) 
+				
+			{
+    				
+				case "show all workers":  
+					
+				{
+						
+					for(int i=0;i<10;i++)
+						
 					{
-						System.out.println("What number?");
-						s = scan.nextLine();
-						worker[Integer.parseInt(s)].ShowHim();
-             					break;
+							
+						worker[i].ShowHim();
+						
 					}
-    				default: 
-					{
-						System.out.println("commands: add worker");
-						System.out.println("          show all workers");
-						System.out.println("          show worker cost");
-						System.exit(0);
-             					break;
-					}
+             					
+					break;
+					
 				}
-
+				
+    				
+				case "add worker":  
+					
+				{
+						
+						
+					AddWorker(worker);
+						
+					System.out.println("Worker was created");
+             				
+					break;
+					
+				}
+				
+				case "show worker cost":  
+					
+				{
+						
+					System.out.println("What number?");
+						
+					s = scan.nextLine();
+						
+					worker[Integer.parseInt(s)].AllCost();
+             					
+					break;
+					
+				}
+    				
+				default: 
+					
+				{
+						
+					System.out.println("commands: add worker");
+						
+					System.out.println("          show all workers");
+						
+					System.out.println("          show worker cost");
+			
+			
+             	break;
+						
+				}
 				
 			}
-        
+
+				
+			
 		}
+        
+		
+	}
+
 
 
 
--- Worker.java
+++ Worker.java
@@ -1,54 +1,99 @@
 package task2_1.worker;
+
 import task2_1.officesupply.OfficeSupply;
+
 //import java.util.Scanner;
 	
 
+
 public class Worker
+	
 {
-	public String name;
-	public OfficeSupply supply[];
-	public Worker()
-	{
-		this.name = "";
-		this.supply = new OfficeSupply[10];
-		for(int i=0; i < 10; i++)
-		supply[i] = new OfficeSupply();
-		//System.out.println("Worker was created");
-	}
+	
+		public String name;
+	
+		public OfficeSupply supply[];
+	
+		public Worker()
+	
+		{
+		
+			this.name = "";
+		
+			this.supply = new OfficeSupply[10];
+		
+			for(int i=0; i < 10; i++)
+		
+			supply[i] = new OfficeSupply();
+		
+			//System.out.println("Worker was created");
+	
+		}
+	
 	
 	public double AllCost()
+			
 	{
+		
 		double all_cost = 0.0;
+		
 		for(int i=0;i<this.supply.length;i++)
+		
 		{
+			
+			
 			all_cost += this.supply[i].cost;
+		
 		}
+		
+		System.out.println("Worker " + this.name + " has items for: " + all_cost);				
 		return all_cost;
+	
 	}
 
+	
 	public void Fill()
+	
 	{
+		
 		
 		for(int i=0;i<this.supply.length;i++)
+		
 		{
+			
 			this.supply[i].name = "some name";
+			
 			this.supply[i].cost = 2.0;
+		
 		}
 		
+	
 	}
 
+	
 	public void ShowHim()
+		
 	{
+		
 		if(name != "")
+		
 		{
+			
 			System.out.println("Worker " + name + " has: ");
+			
 			for(int i=0;i<this.supply.length;i++)
+			
 			{
+				
 				System.out.println("Item " + this.supply[i].name + " which costs: " + this.supply[i].cost);
+			
 			}
+		
 		}
 		
+	
 	}
+
 
 }
 
