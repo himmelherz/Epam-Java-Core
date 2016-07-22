ed042597a982702e0edc98127ac3592846cc42d2
himmelherz <nozarishi@gmail.com>
2016-07-22 16:38:41 +0300

    Task 2_2

---
+++ Main.java
@@ -0,0 +1,66 @@
+package task2_1.main;
+import task2_1.officesupply.OfficeSupply;
+import task2_1.worker.Worker;
+import java.util.Scanner;
+	
+
+public class Main
+{
+
+	public static void AddWorker(Worker[] worker)
+	{
+		Scanner scan = new Scanner(System.in);
+		int k =0;
+		for(int i=0; i< worker.length;i++)
+		{
+			if(worker[i].name.equals("") && (k == 0))
+			{
+				k = 1;
+				System.out.println("Enter name of worker: ");
+				String s = scan.nextLine();
+				worker[i].name = s;
+				worker[i].Fill();
+			}
+		}
+	}
+
+	public static void main(String[] args) 
+		{	
+			OfficeSupply supply = new OfficeSupply();
+			OfficeSupply pen_for_examine = new OfficeSupply();	
+
+			System.out.println(supply.hashCode());
+			System.out.println(supply.toString());
+			System.out.println(supply.equals(pen_for_examine));
+
+			pen_for_examine.cost = 1;
+
+			
+			System.out.println(supply.equals(pen_for_examine));
+
+			String string_for_suicide = new String();		
+
+			System.out.println(supply.equals(string_for_suicide));
+			
+			Worker worker[] = new Worker[10];
+			for(int i=0;i<10;i++)
+			{
+				worker[i] = new Worker();
+				worker[i].Fill();
+			}
+			
+			AddWorker(worker);
+			for(int i=0;i<10;i++)
+			{
+				worker[i].ShowHim();
+			}
+			
+        
+		}
+
+
+
+
+}
+
+	 
---
+++ OfficeSupply.java
@@ -0,0 +1,53 @@
+package task2_1.officesupply;
+//import javase01.t01.log.Log;
+//import java.util.Scanner;
+	
+
+public class OfficeSupply
+{
+	public double cost;
+	public String name;
+
+	public OfficeSupply()
+	{
+		System.out.println("here is the pen");
+		this.name = "";
+		this.cost = 0;
+	}
+	
+	public boolean equals(Object o)
+	{
+		System.out.println("method equals");
+		if ((o != null) && (o.getClass().equals(this.getClass())))
+            	{              
+			System.out.print("method equals: ");
+                	return (((OfficeSupply)o).cost == this.cost) && (((OfficeSupply)o).name == this.name);
+		}else
+			{
+				System.out.print("illegal incoming parameters: ");
+				return false; 
+			}
+
+	}
+
+
+	public int hashCode()
+	{
+		System.out.print("method hashCode: ");
+		return 0;
+	}
+
+	public String toString()
+	{
+		System.out.print("method toString: ");
+		return "Office Supply[" + name + " costs: " + cost + "]";
+	}
+
+	
+
+
+
+
+}
+
+	 
