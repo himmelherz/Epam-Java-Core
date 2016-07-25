c25d2b0f9604cd2c27c12223c685bfbd9642650d
himmelherz <nozarishi@gmail.com>
2016-07-26 02:30:12 +0300

    Now 2_5 with enum, normal arch as ahould be

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,328 @@
+package main;
+
+import course.Course;
+
+import student.Student;
+
+import java.util.Scanner;
+
+import java.util.Arrays;	
+import java.util.*;	
+	
+
+public class Main
+	{
+
+	
+
+		public static void addCourse(ArrayList<Course> courses)
+		{
+			Scanner scan = new Scanner(System.in);
+
+			System.out.println("Enter the course name");
+
+			String s = scan.nextLine();
+
+			switch (s) 
+				
+			{
+    				
+							
+    				
+				case "math":  
+					
+				{
+		
+					Course course = Course.MATH;				
+						
+					courses.add(course);					
+					System.out.println("course was created");
+   
+					System.out.println("___________________");          				
+					break;
+					
+				}
+
+				case "english":  
+					
+				{
+		
+					Course course = Course.ENGLISH;				
+						
+					courses.add(course);					
+					System.out.println("course was created");
+   
+					System.out.println("___________________");          				
+					break;
+					
+				}
+
+				case "it":  
+					
+				{
+		
+					Course course = Course.IT;				
+						
+					courses.add(course);					
+					System.out.println("course was created");
+   
+					System.out.println("___________________");          				
+					break;
+					
+				}
+
+				default: 
+					
+				{
+						
+							
+			
+             	break;
+						
+				}
+				
+			}
+
+
+			
+		}
+			
+		public static void main(String[] args) 
+					
+		{	
+						
+			ArrayList<Course> courses = new ArrayList<Course>();			
+			
+			
+				
+					
+		Scanner scan = new Scanner(System.in);
+	
+		int counter = 0;		
+		while(counter == 0)
+			
+		{
+				
+			System.out.println("Enter your request");
+				
+			String s = scan.nextLine();
+				
+			switch (s) 
+				
+			{
+    				
+							
+    				
+				case "add course":  
+					
+				{
+						
+						
+					addCourse(courses);
+         				
+					break;
+					
+				}
+
+				case "add student":  
+					
+				{
+	
+					System.out.println("What course?");
+				
+					s = scan.nextLine();
+
+					switch(s)
+					{
+						case "math":
+						{
+							for(int i = 0; i < courses.size(); i++)
+							{
+								//Course course = courses.get(i)
+								if(courses.get(i).equals(Course.MATH))
+								{
+									Course course = courses.get(i);
+									course.addStudent();
+									courses.set(i, course);
+								}
+							}
+							break;
+						}
+						case "english":
+						{
+							for(int i = 0; i < courses.size(); i++)
+							{
+								//Course course = courses.get(i)
+								if(courses.get(i).equals(Course.ENGLISH))
+								{
+									Course course = courses.get(i);
+									course.addStudent();
+									courses.set(i, course);
+								}
+							}
+							break;
+						}
+						case "it":
+						{
+							for(int i = 0; i < courses.size(); i++)
+							{
+								//Course course = courses.get(i)
+								if(courses.get(i).equals(Course.IT))
+								{
+									Course course = courses.get(i);
+									course.addStudent();
+									courses.set(i, course);
+								}
+							}
+							break;
+						}
+					}     
+					System.out.println("___________________");  				
+					break;
+					
+				}
+
+				case "show all courses":  
+					
+				{
+						
+						
+					for(int i=0; i< courses.size(); i++)
+					
+{
+						Course course = courses.get(i);
+						course.showAllStudents();
+					}  
+					System.out.println("___________________");      				
+					break;
+					
+				}
+
+				case "find dude":  
+					
+				{
+		
+					ArrayList<Student> dude = new ArrayList<Student>();
+					
+					System.out.println("What's his name?");
+				
+					s = scan.nextLine();
+	
+					System.out.println("Student " + s + " has: "); 			
+						
+					for(int i=0; i< courses.size(); i++)
+					
+{
+						Course course = courses.get(i);
+						for(int j=0; j < course.getList().size(); j++)
+						{
+							if((course.getList().get(j)).name.equals(s))
+							{
+								dude.add(course.getList().get(j));
+								System.out.println(course.getName() + " with " + course.getList().get(j).getGrade()  + " grade ");
+							}
+						}
+
+					}  
+					
+					System.out.println("___________________");      				
+					break;
+					
+				}
+
+				case "standart":  
+					
+				{
+						
+						
+					addCourse(courses);
+
+					addCourse(courses);
+
+					addCourse(courses);
+    
+					for(int z= 0; z< 2;z++)
+					{
+							for(int i = 0; i < courses.size(); i++)
+							{
+								//Course course = courses.get(i)
+								if(courses.get(i).equals(Course.MATH))
+								{
+									Course course = courses.get(i);
+									course.addStudent();
+									courses.set(i, course);
+								}
+							}
+							 				
+					}
+					for(int z= 0; z< 2;z++)
+					{
+							for(int i = 0; i < courses.size(); i++)
+							{
+								//Course course = courses.get(i)
+								if(courses.get(i).equals(Course.IT))
+								{
+									Course course = courses.get(i);
+									course.addStudent();
+									courses.set(i, course);
+								}
+							}
+							 				
+					}
+					for(int z= 0; z< 2;z++)
+					{
+							for(int i = 0; i < courses.size(); i++)
+							{
+								//Course course = courses.get(i)
+								if(courses.get(i).equals(Course.ENGLISH))
+								{
+									Course course = courses.get(i);
+									course.addStudent();
+									courses.set(i, course);
+								}
+							}
+							 				
+					}
+					System.out.println("___________________");
+					break;
+					
+				}
+
+				default: 
+					
+				{
+						
+					System.out.println("commands: add student");
+						
+					System.out.println("          show all students");
+						
+					System.out.println("          show student cost");
+
+					System.out.println("          add discipline");
+
+					System.out.println("          add beginner stuff");
+	
+					System.out.println("          sort student");
+
+					System.out.println("          standart");		
+			
+             	break;
+						
+				}
+				
+			}
+
+				
+			
+		}
+        
+		
+	}
+
+
+
+
+
+
+}
+	 
--- src/main/java/main/Student.java
+++ src/main/java/main/Student.java
@@ -1,8 +1,5 @@
 package student;
 
-import discipline.Discipline;
-
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,243 +9,39 @@ import java.util.List;
 import java.util.*;
 
 
- 	
-class Course
-{
-	Discipline dis;
-	public Course(Discipline dis)
-	{
-		this.dis = dis;	
-	}
-}
 
-public class Student
-	
+public class Student<T1, T2>
 {
 	
 
-   
-	
-
-		
-		public String name;
-	
-		public ArrayList<Course> courses = new ArrayList<Course>();
-		
-		public Student()
-	
-		{
-		
-			this.name = "";
-		
-			//this.supply = new Discipline[10];
-		
-			//for(int i=0; i < 10; i++)
-		
-			//supply[i] = new Discipline();
-		
-			//System.out.println("Worker was created");
-	
-		}
-	
-	public void AddDiscipline()
- 	
- 	{
- 	
- 		int k = 0;
- 		Scanner scan = new Scanner(System.in);
- 		
- 		System.out.println("What do you want to add?");
- 	
- 
- 		String s = scan.nextLine();
- 				
- 		switch (s) 
- 				
- 		{
-     				
- 			case "math":  
- 					
- 			{
-				Discipline dis= Discipline.MATH;
-				
- 				Course math = new Course(dis);				
- 				this.courses.add(math);          					
- 				break;
- 					
- 			}
- 				
-     				
- 			case "it":  
- 					
- 			{
- 						
- 						
- 				Discipline dis= Discipline.IT;
-				
- 				Course i_tech = new Course(dis);				
- 				this.courses.add(i_tech);          					
- 				break;
-   					
- 			}
- 			case "english":  
- 					
- 			{
- 						
- 						
- 				Discipline dis= Discipline.ENGLISH;
-				
- 				Course eng = new Course(dis);				
- 				this.courses.add(eng);          					
- 				break;
-   					
- 			}	
- 			default: 
- 					
- 			{
- 						
- 				System.out.println("Items: pen");
- 						
- 				System.out.println("   notepad");
- 				
- 			        break;
- 						
- 			}
- 				
- 		}
- 
- 		
- 	
- 	}
-	public void setGrade()
+	public T1 name;
+	public T2 grade;
+	public Student()
 	{
-		System.out.println("What's the course?");
- 	
- Scanner scan = new Scanner(System.in);
 	
- 		String s = scan.nextLine();
- 				
- 		switch (s) 
- 				
- 			{
-     				
- 				case "math":  
- 					
- 				{
-					Discipline dis= Discipline.MATH;
- 					Course math = new Course(dis);
-
-
-					Course current = new Course(dis);
-					System.out.println("What's the grade?");
-					s = scan.nextLine();
-					double grade = Double.parseDouble(s);
-					System.out.println(grade);
-					
- 						for(int j = 0; j < this.courses.size(); j++)
-						{
-							current = this.courses.get(j);
- 							if(current.dis.equals(math.dis))
-							{
-								current.dis.setGrade(grade);
-								courses.set(j, current);
-								break;
-							}
- 						}       					
- 					break;
- 					
- 				}
- 				
-     				
- 				case "it":  
- 					
- 				{
- 					Discipline dis= Discipline.IT;
- 					Course i_tech = new Course(dis);
-
-
-					Course current = new Course(dis);
-					System.out.println("What's the grade?");
-					s = scan.nextLine();
-					double grade = Double.parseDouble(s);
-					
- 						for(int j = 0; j < this.courses.size(); j++)
-						{
-							current = this.courses.get(j);
- 							if(current.dis.equals(i_tech.dis))
-							{
-								current.dis.setGrade(grade);
-								courses.set(j, current);
-								break;
-							}
- 						}       					
- 					break;
-   					
- 				}
- 				case "english":  
- 					
- 				{
- 					Discipline dis= Discipline.IT;
- 					Course eng = new Course(dis);
-
-
-					Course current = new Course(dis);
-					System.out.println("What's the grade?");
-					s = scan.nextLine();
-					double grade = Double.parseDouble(s);
-					
- 						for(int j = 0; j < this.courses.size(); j++)
-						{
-							current = this.courses.get(j);
- 							if(current.dis.equals(eng.dis))
-							{
-								current.dis.setGrade(grade);
-								courses.set(j, current);
-								break;
-							}
- 						}       					
- 					break;
-   					
- 				}
- 				default: 
- 					
- 				{
- 						
- 				
- 			        	break;
- 						
- 				}
- 				
- 			}
+	}		
+	public Student(T1 name, T2 grade)
+	{
+		this.name = name;
+		this.grade = grade;
+	}		
+	public T1 getName()
+	{
+		return name;
+	}	
+   	public void setName(T1 name)
+	{
+		this.name = name; 
 	}
-	public void ShowHim()
- 		
- 	{
- 		
- 		if(name != "")
- 		
- 		{
- 			
- 			System.out.println("Student " + name + " has: ");
- 			
- 			for(int i=0;i<this.courses.size();i++)
- 			
- 			{
- 				Discipline dis= Discipline.MATH;
- 				Course cs = new Course(dis);	
- 				cs = this.courses.get(i);
- 				
- 				System.out.println("Course " + cs.dis.toString() + " with grade: " + cs.dis.getGrade());
- 				
- 			}
- 		
- 		}
- 		
- 	
-}
-
 		
-
+	public T2 getGrade()
+	{
+		return grade;
+	}	
+   	public void setGrade(T2 grade)
+	{
+		this.grade = grade; 
+	}
 }
 
 	 
